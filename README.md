1e1cba2e10cb0eebafca500ec6a7b2b5055a893a
rrogerr <rgcbll@gmail.com>
2020-08-08 16:04:37 -0500

    lol

---
+++ README.md
@@ -0,0 +1,43 @@
+**Activate the virtual environment**
+
+```
+source blockchain-env/bin/activate
+```
+
+**Install all packages**
+
+```
+pip install -r requirements.txt
+```
+
+**Run the tests**
+
+Make sure to activate the virtual environment
+
+```
+python3 -m pytest backend/tests
+```
+
+**Run the application and API**
+
+Make sure to activate the virtual environment
+
+```
+python3 -m backend/app
+```
+
+**Run a peer instance**
+
+Make sure to activate the virtual environment
+
+```
+export PEER=True && python3 -m backend.app
+```
+
+**Run the frontend**
+
+In the frontend directory:
+
+```
+npm run start
+```
---
+++ backend/app/__init__.py
@@ -0,0 +1,87 @@
+import os
+import random
+import requests
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+from backend.blockchain.blockchain import Blockchain
+from backend.wallet.wallet import Wallet
+from backend.wallet.transaction import Transaction
+from backend.pubsub import PubSub
+from backend.wallet.transaction_pool import TransactionPool
+
+app = Flask(__name__)
+CORS(app, resources = { r'/*' : { 'origins' : 'http://localhost:3000'} })
+blockchain = Blockchain()
+wallet = Wallet(blockchain)
+transaction_pool = TransactionPool()
+pubsub = PubSub(blockchain, transaction_pool)
+
+@app.route('/')
+def route_default():
+	return 'Welcome to the blockchain'
+
+@app.route('/blockchain')
+def route_blockchain():
+	return jsonify(blockchain.to_json())
+
+@app.route('/blockchain/mine')
+def rout_blockchain_mine():
+	transaction_data = transaction_pool.transaction_data()
+	transaction_data.append(Transaction.reward_transaction(wallet).to_json())
+
+	blockchain.add_block(transaction_data)
+
+	block = blockchain.chain[-1]
+	pubsub.broadcast_block(block)
+
+	transaction_pool.clear_blockchain_transactions(blockchain)
+
+	return jsonify(block.to_json())
+
+@app.route('/wallet/transact', methods = ['POST'])
+def route_wallet_transact():
+	transaction_data = request.get_json()
+	transaction = transaction_pool.existing_transaction(wallet.address)
+
+	if transaction:
+		transaction.update(
+			wallet,
+			transaction_data['recipient'],
+			transaction_data['amount']
+		)
+	else:
+		transaction = Transaction(
+			wallet,
+			transaction_data['recipient'],
+			transaction_data['amount']
+		)
+
+	pubsub.broadcast_transaction(transaction)
+	transaction_pool.clear_blockchain_transactions(blockchain)
+
+	return jsonify(transaction.to_json())
+
+@app.route('/wallet/info')
+def route_wallet_info():
+	return jsonify({'address': wallet.address, 'balance': wallet.balance})
+
+ROOT_PORT = 5000
+PORT = ROOT_PORT
+
+if os.environ.get('PEER') == 'True':
+	PORT = random.randint(5001, 6000)
+
+	result = requests.get(f'http://localhost:{ROOT_PORT}/blockchain')
+	print(f'result.json(): {result.json()}')
+
+	result_blockchain = Blockchain.from_json(result.json())
+
+	try:
+		blockchain.replace_chain(result_blockchain.chain)
+		print('\n -- Successfully synced the local chain')
+	except Exception as e:
+		print(f'\n -- Error syncing: {e}')
+
+app.run(port = PORT)
